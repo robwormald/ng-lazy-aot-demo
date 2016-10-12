26927fdea746c006e269f48ad1b46c22801cc0de
Rob Wormald <robwormald@gmail.com>
2016-10-12 16:27:29 -0700

    build

--- system.build.js
+++ system.build.js
@@ -7,6 +7,6 @@ var routes = [
 
 builder.loadConfig('./system.config.js')
   .then(() => {
-    return builder.build('app - [app/**/*]', 'lib/vendor.js', {minify: true, mangle: true, rollup: true})
-      .then(() => builder.build('app - lib/vendor.js', 'lib/main.js', {minify: true, mangle: true, rollup: true}))
+    return builder.bundle('app - [app/**/*]', 'lib/vendor.js', {minify: true, mangle: true, rollup: true})
+      .then(() => builder.bundle('app - lib/vendor.js', 'lib/main.js', {minify: true, mangle: true, rollup: true}))
   });
